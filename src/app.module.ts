0e8265cbfeef86ece7c42730d24bd365e9e41c69
Spastic <junior.tan@live.com>
2023-05-02 08:18:38 +0800

    initial commit
    nestjs, graphql, prisma

---
+++ src/app.module.ts
@@ -0,0 +1,34 @@
+import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+
+import { ApolloServerPluginLandingPageLocalDefault } from '@apollo/server/plugin/landingPage/default';
+import { join } from 'path';
+import { PrismaModule } from './prisma/prisma.module';
+
+@Module({
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+
+      // https://www.youtube.com/watch?v=J7V9OhIphbY
+      // autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      // sortSchema: true,
+
+      // https://www.youtube.com/watch?v=twi33GVRazE&t=347s
+      playground: false,
+      plugins: [ApolloServerPluginLandingPageLocalDefault()],
+      typePaths: ['./**/*.graphql'],
+      definitions: {
+        path: join(process.cwd(), 'src/types/graphql.ts'),
+        outputAs: 'class'
+      },
+    }),
+    PrismaModule
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule { }
